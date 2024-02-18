1880ef3bbbaf6b154522ff373354a573f8edb6d3
Anthony Zegnal <anthonyzegnal05@gmail.com>
2024-02-18 15:41:44 +0100

    j'ai fait une page avec des infos et la page de pres du site

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,21 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                    <!-- Section pour afficher les informations de l'utilisateur -->
+                    <div class="mt-4">
+                        <h2>{{ __('Informations de l\'utilisateur') }}</h2>
+                        <p>{{ __("Pseudo: ") }} {{ Auth::user()->name }}</p>
+                        <p>{{ __("Âge: ") }} {{ Auth::user()->age }}</p>
+                        <p>{{ __("Sexe: ") }} {{ Auth::user()->sex }}</p>
+                        <p>{{ __("Poids: ") }} {{ Auth::user()->poids }}</p>
+                        <p>{{ __("Taille: ") }} {{ Auth::user()->taille }}</p>
+                    </div>
+
+                    <!-- Section pour afficher les tâches quotidiennes de l'utilisateur -->
+                    <div class="mt-4">
+                        <h2>{{ __('Tâches quotidiennes') }}</h2>
+                    </div>
                 </div>
             </div>
         </div>
