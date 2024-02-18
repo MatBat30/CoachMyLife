7924a64a1fe753a5b18f6b3096006764af171309
mathis <mathisbatailler30@gmail.>
2024-02-18 14:37:45 +0100

    migrate et models user +debuts exercices++

---
+++ database/seeders/ExerciseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class ExerciseSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('exercises')->insert([
+            ['nom' => 'Push-ups'],
+            ['nom' => 'Pull-ups'],
+            ['nom' => 'Squats'],
+            ['nom' => 'Leg Raises'],
+            ['nom' => 'Planks'],
+            [ 'nom' => 'Jumping Jacks'],
+            [ 'nom' => 'Lunges'],
+            [ 'nom' => 'Burpees'],
+            [ 'nom' => 'High Knees'],
+            [ 'nom' => 'Mountain Climbers']
+        ]);
+
+    }
+}
